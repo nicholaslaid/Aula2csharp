38ffae6b5951a682d4ce0f8c84e093dd3249aa59
A1bel <Arthur.Aibel1@gmail.com>
2023-05-18 21:27:43 -0300

    Delete Implementado

--- Aula2/Form1.cs
+++ Aula2/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Eventing.Reader;
+
 namespace Aula2
 {
     public partial class Form1 : Form
@@ -12,18 +14,24 @@ namespace Aula2
         {
             product product = new product();
 
-            product.name = textBox3.Text;
-            product.model = textBox4.Text;
-            product.quantity = Convert.ToInt32(textBox1.Text);
-            product.value = float.Parse(textBox2.Text);
+            product.name = txtName.Text;
+            product.model = txtModel.Text;
+            product.quantity = Convert.ToInt32(txtQuantity.Text);
+            product.value = float.Parse(txtValue.Text);
 
 
             bool response = product.Add(product);
 
 
             if (!response)
+            {
                 MessageBox.Show("Erro ao tentar gravar o produto");
 
+            }
+            else
+            {
+                LoadProducts();
+            }
         }
 
         private void LoadProducts()
@@ -31,7 +39,62 @@ namespace Aula2
             product product = new product();
             dgvProdutos.DataSource = product.GetALL();
         }
+
+        private void dgvProdutos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                product producto = new product();
+
+                int id = Convert.ToInt32(
+                    dgvProdutos.Rows[dgvProdutos.CurrentRow.Index].Cells[0].Value);
+
+                producto = producto.Get(id);
+
+                txtId.Text = producto.id.ToString();
+                txtName.Text = producto.name.ToString();
+                txtModel.Text = producto.model.ToString();
+                txtQuantity.Text = producto.quantity.ToString();
+                txtValue.Text = producto.value.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao tentar carregar os dados do produto");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dialog = MessageBox.Show("Confirma exclusão?", "Excluir",
+                                                      MessageBoxButtons.YesNo,
+                                                      MessageBoxIcon.Question);
+
+                if(dialog == DialogResult.Yes)
+                {
+                    product product = new product();
+                    int id = Convert.ToInt32(txtId.Text);
+                    bool response = product.Delete(id);
+
+                    if(!response) {
+                        MessageBox.Show("Erro ao tentar excluir");
+                    }
+                    else
+                    {
+                        LoadProducts();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
+
+
+        }
     }
 
-   
+
 }
